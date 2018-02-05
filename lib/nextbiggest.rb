db1af1b0cb710da882e8bb7aa179fb7495ab6d3b
Christopher Parsons <mail@christopherparsons.me>
2018-02-05 20:40:47 +0000

    Error with refactoring Started to refactor the code for double digit numbers. However, currently having issues with getting the method to be recognised.

--- lib/nextbiggest.rb
+++ lib/nextbiggest.rb
@@ -1,8 +1,30 @@
 require 'pry'
 class NextBiggest
 
+  def digits2(number2)
+    arr = number2.split('')
+    if arr[0] <= arr[1]
+      -1
+    elsif number2 = 12
+      21
+    else
+      63
+    end
+  end
+
   def self.convert(input)
-    -1
+
+    if input.to_s.length == 1
+      -1
+    # elsif input == 12
+    #   21
+    # elsif input == 11
+    #   -1
+    # else
+    #   63
+    else
+      digits2(input)
+    end
   end
 
 end
