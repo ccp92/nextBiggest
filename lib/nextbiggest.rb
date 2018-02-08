cb80183fd138e391e585a5b657c6cb2c3a58d0d1
Christopher Parsons <mail@christopherparsons.me>
2018-02-08 18:13:27 +0000

    Minor Refactoring Tidied up some of my code and refactored part of my if into a seperate method.

--- lib/nextbiggest.rb
+++ lib/nextbiggest.rb
@@ -1,33 +1,25 @@
 require 'pry'
 class NextBiggest
 
-  # def digits2(number2)
-  #   arrDigits = number2.to_s.split('')
-  #   arrDigits.map! {|x| x.to_i}
-  #   if arrDigits[0] >= arrDigits[1]
-  #     -1
-  #   else
-  #     arrDigits.reverse.join.to_i
-  #   end
-  # end
-
   def self.convert(input)
     stringput = input.to_s
     # stringified version of the integer input
     if stringput.length == 1
       -1
     else
-      # digits2(input)
-      arrDigits = stringput.split('')
-      # split the digits into an array (they are stored as strings)
-      arrDigits.map! {|x| x.to_i}
-      # this is remapping over the array and turning the array of strings
-      if arrDigits[0] >= arrDigits[1]
-        -1
-      else
-        arrDigits.reverse.join.to_i
-      end
+      convert_two_digits(stringput)
     end
   end
 
+  private
+
+  def self.convert_two_digits(input)
+    array_of_digits = input.split('')
+    array_of_digits.map! {|x| x.to_i}
+    if array_of_digits[0] >= array_of_digits[1]
+      -1
+    else
+      array_of_digits.reverse.join.to_i
+    end
+  end
 end
